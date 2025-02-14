a544e3b10fb91ded9ff16759e8e457c9fe30229b
Tracky06 <ro142857@gmail.com>
2025-02-14 01:23:01 +0000

    another commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/Tenzies/'
+})
 
-export default {
-  base: '/Tenzies/', // Replace <repository-name> with the name of your GitHub repo
-}
